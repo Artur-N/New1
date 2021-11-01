f9b5a6a556316a8f3c50ee117dbd5ba325d33470
Artur <arturnl@ya.ru>
2021-11-02 00:42:54 +0300

    Dog +

--- Prog04/Program.cs
+++ Prog04/Program.cs
@@ -3,25 +3,26 @@ d = 10000; // Distance
 a = 1; // Speed 1
 b = 2; // Speed 2
 c = 5; // Speed dog
-s = (1, 2);
+s = 2;
 int count = 0;
-int time = 0;
+int time;
 
-while (d >= 10)
+while (d > 5)
 {
-    if (s = 1)
+    if (s == 1)
     {
         time = d / (a + c);
         s = 2;
     }
- 
-    if (s = 2)
+
+    else
     {
         time = d / (b + c);
         s = 1;
     }
 
     d = d - (a + b) * time;
-    count = count + 1;
+    count++;
 }
 Console.WriteLine(count);
+Console.WriteLine(d);
